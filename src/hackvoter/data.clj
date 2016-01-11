92067ae268e3bbe717c93923bf8e36ba3e4f1a2c
Steve Robbins <sr@mixrad.io>
2016-01-11 21:58:26 +0000

    diagnosis

--- src/hackvoter/data.clj
+++ src/hackvoter/data.clj
@@ -115,24 +115,17 @@
 
 (defn store-vote [userid publicid votes]
 	; validate the allocation in case some smart-ass uses jquery to post bad votes :)
-  (warn (str "store-vote userid=" userid " publicid=" publicid " votes=" votes " "))
+  (warn (str "store-vote userid=" userid " publicid=" publicid " votes=" votes " " (class votes)))
 	  (let [config (get-config-items)
   				allowvoting (:allowvoting config)
   				allocation (:allocation config)
   				maxspend (:maxspend config)
-  				existingvotes (get-user-votes userid)]
-          (warn (str "store-vote " allowvoting " " allocation " " maxspend " " (apply str existingvotes)))
+  				existingvotes (get-user-votes userid)
+          uservotesincludingthis (+ votes (reduce + (map (fn[x] (if (zero? (compare publicid (:id x))) 0 (:uservotes x))) existingvotes)))]
+          (warn (str "store-vote " allowvoting " " allocation " " maxspend " " (apply str existingvotes) " " uservotesincludingthis " " (class uservotesincludingthis) " " maxspend " " (class maxspend) " " allocation " " (class allocation)))
           (let[
-  				uservotesincludingthis (+ votes 
-            (reduce + 
-              (map 
-                (fn[x] 
-                  (if (zero? (compare publicid (:id x)))
-                    0
-                    (:uservotes x)))
-                existingvotes)))
-  				votewithinbudget (and (<= votes maxspend) (<= uservotesincludingthis allocation))
-  				oktostore (and allowvoting votewithinbudget)]
+    				votewithinbudget (and (<= votes maxspend) (<= uservotesincludingthis allocation))
+    				oktostore (and allowvoting votewithinbudget)]
   		(warn (str "votewithinbudget=" votewithinbudget " uservotesincludingthis=" uservotesincludingthis))
   		(when oktostore
   			(prn (str "store-vote userid=" userid " publicid=" publicid " votes=" votes))
